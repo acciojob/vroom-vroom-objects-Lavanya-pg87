2cb60770ba313c3d6d52a948077ea19c7eb49c3f
Lavanya-pg87 <lavanyagangadharan99@gmail.com>
2024-09-18 18:42:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+car.window.getMakeModel = function(){
+	return `${this.make} ${this.model}`;
+}
+
+function SportsCar(make, model, topSpeed) {
+	car.call(this, make, model);
+	this.toSpeed = topSpeed;
+}
 
 // Do not change the code below
 window.Car = Car;
